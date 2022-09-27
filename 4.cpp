9f91d21255ffcce490e1aeddcdfaf22ef367ac29
maitriyanamettallia <114065104+maitriyanamettallia@users.noreply.github.com>
2022-09-27 19:59:40 +0700

    copy data dokumen

---
+++ 4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int pil; cout<<"pil: "; cin>>pil;
+	switch (pil){
+		case 1: cout<<"algoritma"<<endl;
+		cout<<"dan"<<endl;
+		cout<<"pemrograman"<<endl;
+		break;
+		case 2: cout<<"matematika"<<endl;
+		cout<<"diskrit"<<endl;
+		break;
+		default: cout<<"bahasa"<<endl;
+		cout<<"inggris"<<endl;
+	}
+}
