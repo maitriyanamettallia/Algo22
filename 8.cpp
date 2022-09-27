b5292404dbd799ed78a2ef196b653ad83490eaf5
maitriyanamettallia <114065104+maitriyanamettallia@users.noreply.github.com>
2022-09-27 21:40:23 +0700

    copy data dokumen

---
+++ 8.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int u;
+	char lagi;
+	atas:
+	cout<<"masukkan umur: ";
+	cin>>u;
+	
+	cout<<"umur anda adalah: "<<u<<endl;
+	cout<<"ulang lagi (y/t): ";
+	cin>>lagi;
+	
+	if(lagi == 'y'){
+		goto atas;
+	}
+}
---
+++ 9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int c=1;
+	while (c<= 10){
+		cout<<c<<endl;
+		c++;
+	}
+}
